712af83cd32996d89b1c8de2728e9a5e055e47df
Cherise <nashpaiessy@gmail.com>
2023-09-08 11:18:25 +0300

    done

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,30 @@
+import ipdb
 class MySet:
+    def __init__(self,  enumerable = []):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
+            ipdb.set_trace()
 
-    pass
+    def has(self, value):
+        return value in self.dictionary
+    
+    def add(self,value):
+        self.dictionary[value] = True
+        return self
+    
+    def delete(self,value):
+        self.dictionary.pop(value, None)
+        return self
+    
+    def size(self):
+        return len(self.dictionary)
+    
+    def clear(self):
+        self.dictionary.clear()
+        return self
+    
+    def str(self):
+        print(str(self.dictionary))
+
+set = MySet([1, 2, 3, 3])
